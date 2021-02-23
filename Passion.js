d0cac5219a926c5939003381f9a69b90ba15664d
ShowCaseCode <showcasecode@outlook.com>
2021-02-23 17:42:07 -0500

    My Fifth Commit

--- Passion.js
+++ Passion.js
@@ -8,6 +8,12 @@ let EndPoint = "REST API";
 
 console.log(EndPoint);
 
-if (DevPlatform == "Cisco Dev Platform")
-    console.log("Join Cisco Dev Platform")
-    console.log("Do not wait.  Do it now!!!")
+if (DevPlatform == "Cisco Dev Platform");
+    console.log("Join Cisco Dev Platform");
+    console.log("Do not wait.  Do it now!!!");
+    console.log("Always study and hit your books, hard");
+
+
+let greetings = "Take Care!";
+
+console.log(greetings);
