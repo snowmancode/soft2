ba433a1cc50656ac09dbaf2ac172ce2b3b2461d0
ShowCaseCode <showcasecode@outlook.com>
2021-02-28 15:54:37 -0500

    3rd commit for Feb 28, 2021

---
+++ WickedEyes.py
@@ -0,0 +1,12 @@
+my_cars = {
+    "brand": "Ford",
+    "model": "F-150",
+    "year": 2001,
+
+    "brand": "Ford",
+    "model": "Focus",
+    "year": 2002
+}
+
+print(len(my_cars));
+print(my_cars);
--- WickedQuotes.py
+++ WickedQuotes.py
@@ -6,3 +6,16 @@ print(villians);
 dc_villians = ("The Joker", "The Riddler", "Bane");
 print(len(dc_villians));
 print(dc_villians);
+
+my_cars = {
+    "brand": "Ford",
+    "model": "F-150",
+    "year": 2001,
+
+    "brand": "Ford",
+    "model": "Focus",
+    "year": 2002
+}
+
+print(len(my_cars));
+print(my_cars);
