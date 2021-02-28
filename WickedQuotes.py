25744fb2d7d8729a1d8f02cd7bc3208be8c604b8
ShowCaseCode <showcasecode@outlook.com>
2021-02-28 15:31:04 -0500

    2nd commit for Feb 28, 2021

--- WickedQuotes.py
+++ WickedQuotes.py
@@ -2,3 +2,7 @@ villians = ["Darkside", "Doomsday", "Lex Luthor"];
 
 print(len(villians));
 print(villians);
+
+dc_villians = ("The Joker", "The Riddler", "Bane");
+print(len(dc_villians));
+print(dc_villians);
