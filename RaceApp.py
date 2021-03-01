e623a8505908ea0658bd00f7803368fd01aa4519
ShowCaseCode <showcasecode@outlook.com>
2021-03-01 11:57:02 -0500

    1st commit for March 1, 2021

---
+++ RaceApp.py
@@ -0,0 +1,14 @@
+name = input("What is your name? ");
+occupation = input("What do you do for a living? ");
+hobby = input("What is your hobby? ");
+ethnicity = input("What is your ethnicity? ");
+
+fancy_line = "=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0=0";
+print("\n\n\n");
+print(fancy_line);
+print("\n", name);
+print("\n", occupation);
+print("\n", hobby);
+print("\n", ethnicity);
+print("\n\n");
+print(fancy_line);
