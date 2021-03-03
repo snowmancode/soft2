54f7b4276403735dfc87129d411b81a5398193dc
ShowCaseCode <showcasecode@outlook.com>
2021-03-03 00:52:52 -0500

    3rd commit for March 3, 2021

--- testingAPIs.py
+++ testingAPIs.py
@@ -4,3 +4,9 @@ the_response = requests.get(url="http://http://api.open-notify.org/iss-now.json"
 
 print(the_response)
 print(response.status_code)
+
+txt = "Welcome to my Kingdom"
+
+a = txt.split()
+
+print(a)
